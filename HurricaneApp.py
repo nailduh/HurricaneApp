03a6a4857460a9ce023e87fa1df7989174dea6df
Sierrag1425 <gxs6911@mavs.uta.edu>
2024-10-12 16:38:17 -0500

    Messing with steamlit. Added an input for location information and added information.

--- HurricaneApp.py
+++ HurricaneApp.py
@@ -3,3 +3,9 @@ import streamlit as st
 import pandas as pd
 
 st.title("Hurricane App")
+st.html("<p>Produced by Jr and Nelda</span></p>")
+st.divider()
+st.header("Infomation")
+st.write("We want to know what your location is to be able to give you the most accurate data")
+user_location = st.text_input("Location: ")
+st.divider()
